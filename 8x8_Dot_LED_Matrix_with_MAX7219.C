c85bce799d2bc5f9ddc362d09b935dba4c21e09b
Félix Chenette-Stewart <felix@nexumnet.com>
2020-12-12 04:24:59 -0500

    Add files via upload

---
+++ 8x8_Dot_LED_Matrix_with_MAX7219.C
@@ -0,0 +1,32 @@
+#include <LedControl.h>
+int DIN = 12;
+int CS =  11;
+int CLK = 10;
+byte c[8]=     {0x7E,0x7E,0x60,0x60,0x60,0x60,0x7E,0x7E};
+byte eight[8]= {0x7E,0x7E,0x66,0x7E,0x7E,0x66,0x7E,0x7E};
+byte s[8]=     {0x7E,0x7C,0x60,0x7C,0x3E,0x06,0x3E,0x7E};
+byte dot[8]=   {0x00,0x00,0x00,0x00,0x00,0x00,0x18,0x18};
+byte o[8]=     {0x7E,0x7E,0x66,0x66,0x66,0x66,0x7E,0x7E};
+LedControl lctrol=LedControl(DIN,CLK,CS,0);
+void setup(){
+ lctrol.shutdown(0,false);
+ lctrol.setIntensity(0,15);
+ lctrol.clearDisplay(0);
+}
+
+void loop(){
+    printByte();
+    delay(500);
+}
+
+void printByte(byte character [])
+{
+    int i = 0;
+    for(i=0;i<8;i++)
+    {
+       lctrol.setRow(0,i,character[i]);
+    }
+}
+
+//https://github.com/wayoda/LedControl
+//https://www.arduino.cc/en/Guide/Libraries
